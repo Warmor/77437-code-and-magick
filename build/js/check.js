8f3754f256d3ea679e0bc42085fbe3098f874ed5
Warmor <grishin.pd@gmail.com>
2016-03-30 00:42:38 +0300

    Вывод сообщений

--- build/js/check.js
+++ build/js/check.js
@@ -13,16 +13,17 @@ var getMessage = function(a, b) {
 	} else if (typeof a === 'number') {
 		mesage = ('Я прыгнул на ' + a * 100 + ' сантиметров');
 	} else if (Array.isArray(a) && Array.isArray(b)) {
-			for (var i = 0; i < a.length; i++) {
-				length = length + a[i] + b[i];
-			}
-			mesage = 'Я прошёл ' + length + ' метров';
-		} else if (Array.isArray(a) && !Array.isArray(b)){
-			for (var i = 0; i < a.length; i++) {
-				sum += a[i];
-			}
-			mesage = 'Я прошёл ' + sum + ' шагов';
+		for (var i = 0; i < a.length; i++) {
+			length = length + a[i] + b[i];
 		}
+		mesage = 'Я прошёл ' + length + ' метров';
+	} else if (Array.isArray(a) && !Array.isArray(b)){
+		for (var i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+		mesage = 'Я прошёл ' + sum + ' шагов';
 	}
-	return mesage;
+return mesage;	
 };
+	
+
