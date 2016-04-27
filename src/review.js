b834beacd4df4a34fdd34bb5ebe6f3bb1d28f6f2
Warmor <grishin.pd@gmail.com>
2016-04-28 01:58:54 +0300

    module8-task1

---
+++ src/review.js
@@ -0,0 +1,39 @@
+'use strict';
+
+define('review', ['./review-create'], function(reviewCreate) {
+
+  function Review(data) {
+    var quiz;
+
+    this.data = data;
+    this.element = reviewCreate(this.data);
+
+    this.quiz = this.element.querySelector('.review-quiz');
+
+    quiz = this.quiz;
+
+    this.onQuizClick = function(evt) {
+      var quizActive = quiz.querySelector('.review-quiz-answer-active');
+
+      if (evt.target.classList.contains('review-quiz-answer')) {
+        if (quizActive) {
+          quizActive.classList.remove('review-quiz-answer-active');
+        }
+
+        evt.target.classList.add('review-quiz-answer-active');
+      }
+    };
+
+    this.remove = function() {
+      this.quiz.removeEventListener('click', this.onQuizClick);
+      this.element.parentNode.removeChild(this.element);
+    };
+
+    this.quiz.addEventListener('click', this.onQuizClick);
+  }
+
+
+  return Review;  
+});
+
+
