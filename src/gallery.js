83277f29e50f779c9a49b7fbc48671a78c35dff0
Warmor <grishin.pd@gmail.com>
2016-05-01 22:07:27 +0300

    module9-task3

--- src/gallery.js
+++ src/gallery.js
@@ -3,8 +3,6 @@
 define('gallery', function() {
 
   function Gallery(overlay) {
-    var self = this;
-
     this.overlayGallery = overlay.gallery;
     this.overlayGalleryClose = overlay.galleryClose;
     this.overlayGalleryPreview = overlay.galleryPreview;
@@ -13,85 +11,91 @@ define('gallery', function() {
     this.overlayNumberCurrent = overlay.galleryNumberCurrent;
     this.overlayNumberTotal = overlay.galleryNumberTotal;
     this.overlayGalleryPhoto = new Image();
-    self.overlayGalleryPhoto.style.maxHeight = 740 + 'px';
+    this.overlayGalleryPhoto.style.maxHeight = 740 + 'px';
     this.overlayListSrc = overlay.galleryListSrc;
     this.numberImg = 0;
+    this._tooglePhoto = this._tooglePhoto.bind(this); 
+    this._showNumberCurrent = this._showNumberCurrent.bind(this); 
+    this._isPhoto = this._isPhoto.bind(this); 
+    this._showGallery = this._showGallery.bind(this); 
+    this._onDocumentKeyDown = this._onDocumentKeyDown.bind(this); 
+    this._hideGallery = this._hideGallery.bind(this); 
+    this.setHash = this.setHash.bind(this); 
+    this.getHash = this.getHash.bind(this); 
+  };
 
+  Gallery.prototype._tooglePhoto = function(state) {
+    if (this.overlayGalleryNext === state.target) {
+      this.numberImg++;
+    } else {
+      this.numberImg--;
+    }
+    this.setHash(this.overlayListSrc[this.numberImg]);
+  };
 
-    this._tooglePhoto = function(state) {
-      if (self.overlayGalleryNext === state.target) {
-        self.numberImg++;
-      } else {
-        self.numberImg--;
+  Gallery.prototype._showNumberCurrent = function() {
+    this.overlayNumberCurrent.innerHTML = +(this.numberImg) + 1;
+    this.overlayNumberTotal.innerHTML = this.overlayListSrc.length;
+    if (this.numberImg < 1) {
+      this.overlayGalleryPrev.classList.add('invisible');
+    }
+    if (this.numberImg > this.overlayListSrc.length - 2) {
+      this.overlayGalleryNext.classList.add('invisible');
+    }
+    if (this.numberImg > 0 && this.numberImg < this.overlayListSrc.length - 1) {
+      this.overlayGalleryPrev.classList.remove('invisible');
+      this.overlayGalleryNext.classList.remove('invisible');
+    }
+  };
+
+  Gallery.prototype._isPhoto = function(url) {
+    for (var i = 0; i < this.overlayListSrc.length; i++) {
+      if (this.overlayListSrc[i] === url.slice(7)) {
+        this.numberImg = i;
+        this.overlayGalleryPhoto.src = this.overlayListSrc[this.numberImg];
+        this._showNumberCurrent(i);
       }
-      self.setHash(self.overlayListSrc[self.numberImg]);
-    };
+    }
+  };
 
-    this._showNumberCurrent = function() {
-      self.overlayNumberCurrent.innerHTML = +(self.numberImg) + 1;
-      self.overlayNumberTotal.innerHTML = self.overlayListSrc.length;
-      if (self.numberImg < 1) {
-        self.overlayGalleryPrev.classList.add('invisible');
-      }
-      if (self.numberImg > self.overlayListSrc.length - 2) {
-        self.overlayGalleryNext.classList.add('invisible');
-      }
-      if (self.numberImg > 0 && self.numberImg < self.overlayListSrc.length - 1) {
-        self.overlayGalleryPrev.classList.remove('invisible');
-        self.overlayGalleryNext.classList.remove('invisible');
-      }
-    };
+  Gallery.prototype._showGallery = function(url) {
+    this.overlayGallery.classList.remove('invisible');
+    this.overlayGalleryPreview.appendChild(this.overlayGalleryPhoto);
+    this._isPhoto(url);
+    this.overlayGalleryPrev.addEventListener('click', this._tooglePhoto);
+    this.overlayGalleryNext.addEventListener('click', this._tooglePhoto);
+    this.overlayGalleryClose.addEventListener('click', this._hideGallery);
+    window.addEventListener('keydown', this._onDocumentKeyDown);
+  };
 
-    this._isPhoto = function(url) {
-      for (var i = 0; i < self.overlayListSrc.length; i++) {
-        if (self.overlayListSrc[i] === url.slice(7)) {
-          self.numberImg = i;
-          self.overlayGalleryPhoto.src = self.overlayListSrc[self.numberImg];
-          self._showNumberCurrent(i);
-        }
-      }
-    };
+  Gallery.prototype._onDocumentKeyDown = function(evt) {
+    if (evt.keyCode === 27) {
+      this._hideGallery();
+    }
+  };
 
-    this._showGallery = function(url) {
-      self.overlayGallery.classList.remove('invisible');
-      self.overlayGalleryPreview.appendChild(self.overlayGalleryPhoto);
-      self._isPhoto(url);
-      this.overlayGalleryPrev.addEventListener('click', self._tooglePhoto);
-      this.overlayGalleryNext.addEventListener('click', self._tooglePhoto);
-      this.overlayGalleryClose.addEventListener('click', self._hideGallery);
-      window.addEventListener('keydown', self._onDocumentKeyDown);
-    };
+  Gallery.prototype._hideGallery = function() {
+    this.setHash();
+    this.overlayGallery.classList.add('invisible');
+    this.overlayGalleryPrev.removeEventListener('click', this._tooglePhoto);
+    this.overlayGalleryNext.removeEventListener('click', this._tooglePhoto);
+    this.overlayGalleryClose.removeEventListener('click', this._hideGallery);
+    window.removeEventListener('keydown', this._onDocumentKeyDown);
+  };
 
-    this._onDocumentKeyDown = function(evt) {
-      if (evt.keyCode === 27) {
-        self._hideGallery();
-      }
-    };
+  Gallery.prototype.setHash = function(url) {
+    if(url) {
+      location.hash = '#photo/' + url;
+    } else {
+      location.hash = '';
+    }
+  };
 
-    this._hideGallery = function() {
-      self.setHash();
-      self.overlayGallery.classList.add('invisible');
-      self.overlayGalleryPrev.removeEventListener('click', self._tooglePhoto);
-      self.overlayGalleryNext.removeEventListener('click', self._tooglePhoto);
-      self.overlayGalleryClose.removeEventListener('click', self._hideGallery);
-      window.removeEventListener('keydown', self._onDocumentKeyDown);
-    };
-
-    this.setHash = function(url) {
-      if(url) {
-        location.hash = '#photo/' + url;
-      } else {
-        location.hash = '';
-      }
-    };
-
-    this.getHash = function(url) {
-      if (~url.indexOf('#photo/')) {
-        self._showGallery(url);
-      }
-    };
-  }
-
+  Gallery.prototype.getHash = function(url) {
+    if (~url.indexOf('#photo/')) {
+      this._showGallery(url);
+    }
+  };
   return Gallery;
 
 });
--- src/review.js
+++ src/review.js
@@ -3,35 +3,28 @@
 define('review', ['./review-create'], function(reviewCreate) {
 
   function Review(data) {
-    var quiz;
-
     this.data = data;
     this.element = reviewCreate(this.data);
-
     this.quiz = this.element.querySelector('.review-quiz');
-
-    quiz = this.quiz;
-
-    this.onQuizClick = function(evt) {
-      var quizActive = quiz.querySelector('.review-quiz-answer-active');
-
-      if (evt.target.classList.contains('review-quiz-answer')) {
-        if (quizActive) {
-          quizActive.classList.remove('review-quiz-answer-active');
-        }
-
-        evt.target.classList.add('review-quiz-answer-active');
-      }
-    };
-
-    this.remove = function() {
-      this.quiz.removeEventListener('click', this.onQuizClick);
-      this.element.parentNode.removeChild(this.element);
-    };
-
+    this.onQuizClick = this.onQuizClick.bind(this);
+    this.remove = this.remove.bind(this);
     this.quiz.addEventListener('click', this.onQuizClick);
-  }
+  };
 
+  Review.prototype.onQuizClick = function(evt) {
+    var quizActive = this.quiz.querySelector('.review-quiz-answer-active');
 
+    if (evt.target.classList.contains('review-quiz-answer')) {
+      if (quizActive) {
+        quizActive.classList.remove('review-quiz-answer-active');
+      }
+      evt.target.classList.add('review-quiz-answer-active');
+    }
+  };
+
+  Review.prototype.remove = function() {
+    this.quiz.removeEventListener('click', this.onQuizClick);
+    this.element.parentNode.removeChild(this.element);
+  };
   return Review;
 });
